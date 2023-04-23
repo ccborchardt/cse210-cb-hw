b2bea44cd161712f347622b954489199510c6445
ccborchardt <chantalborchardt@gmail.com>
2023-04-22 22:01:28 -0600

    4/22 Pt 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string cbfirst = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string cblast = Console.ReadLine();
+
+        Console.WriteLine(" ");
+
+        Console.WriteLine($"Your name is {cblast}, {cbfirst} {cblast}. ");
     }
 }
