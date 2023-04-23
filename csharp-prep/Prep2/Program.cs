9adddf97660bbcc57e17f1d0b1accf199956a26e
ccborchardt <chantalborchardt@gmail.com>
2023-04-22 22:15:22 -0600

    4/22 pt 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int p = int.Parse(grade);
+
+        string letter = "";
+
+        if (p >= 90)
+        {
+            letter = "A";
+        }
+        else if (p >= 80)
+        {
+            letter = "B";
+        }
+        else if (p >= 70)
+        {
+            letter = "C";
+        }
+        else if (p >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Letter Grade: {letter}");
+        
     }
 }
