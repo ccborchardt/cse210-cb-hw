888fcb8d31480b6e2aa68ce6a7661412411787c6
ccborchardt <chantalborchardt@gmail.com>
2023-05-03 21:30:32 -0600

    5/3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,16 +7,29 @@ namespace Learning02
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Learning02 World!");
-            Learning02.Program p = new Learning02.Program();
+            Job job1 = new Job();
+            job1._jobTitle = "Software Engineer";
+            job1._company = "Microsoft";
+            job1._startYear = 2019;
+            job1._endYear = 2022;
+
+            Job job2 = new Job();
+            job2._jobTitle = "Manager";
+            job2._company = "Apple";
+            job2._startYear = 2022;
+            job2._endYear = 2023;
+
+
+            Resume _resume = new Resume();
+            _resume._name = "Allison Rose";
+            _resume._jobList.Add(job1);
+            _resume._jobList.Add(job2);
+
+            _resume.DisplayResumeInfo();
+
+
            
         }
     }
 }
 
-public class Program1 
-{
-    public void DoSome()
-    {
-        Console.WriteLine("How I'm inside the other program class.");
-    }
-}
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobList = new List<Job>();
+
+    public void DisplayResumeInfo()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobList)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+
+}
